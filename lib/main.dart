2fe55ee3f650b91f52708783de6c308c1ffe5c9a
Akshar Kalathiya <147808278+Akshar062@users.noreply.github.com>
2023-11-18 13:59:36 +0530

    App with UI And Two Units

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,138 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'dart:math';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Speedometer App',
+      home: SpeedometerPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class SpeedometerPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _SpeedometerPageState createState() => _SpeedometerPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SpeedometerPageState extends State<SpeedometerPage> {
+  Location location = Location();
+  double? _speed;
+  String _selectedSpeedUnit = 'm/s';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _checkLocationPermission();
+  }
+
+  Future<void> _checkLocationPermission() async {
+    if (await location.hasPermission() != true) {
+      await location.requestPermission();
+    }
+    _getLocation();
+  }
+
+  void _getLocation() {
+    location.onLocationChanged.listen((LocationData currentLocation) {
+      setState(() {
+        _speed = currentLocation.speed;
+      });
     });
+  }
+
+  void _changeSpeedUnit(String? unit) {
+    if (unit != null) {
+      setState(() {
+        _selectedSpeedUnit = unit;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Speedometer App'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CustomPaint(
+            painter: SpeedometerPainter(_speed ?? 0, _selectedSpeedUnit),
+            size: const Size(200, 200),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            'Speed: ${_speed ?? 0} $_selectedSpeedUnit',
+            style: const TextStyle(fontSize: 20),
+          ),
+          const SizedBox(height: 20),
+          DropdownButton<String>(
+            value: _selectedSpeedUnit,
+            onChanged: _changeSpeedUnit,
+            items: ['m/s', 'km/h'].map((unit) {
+              return DropdownMenuItem<String>(
+                value: unit,
+                child: Text(unit),
+              );
+            }).toList(),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class SpeedometerPainter extends CustomPainter {
+  final double speed;
+  final String speedUnit;
+
+  SpeedometerPainter(this.speed, this.speedUnit);
+
+  double degreesToRadians(double degrees) {
+    return degrees * (pi / 180.0);
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint circlePaint = Paint()
+      ..color = Colors.blue
+      ..strokeWidth = 8.0
+      ..style = PaintingStyle.stroke;
+
+    final Paint arrowPaint = Paint()
+      ..color = Colors.red
+      ..strokeWidth = 4.0
+      ..style = PaintingStyle.stroke;
+
+    final double centerX = size.width / 2;
+    final double centerY = size.height / 2;
+    final double radius = min(centerX, centerY) - 20;
+
+    // Draw the speedometer circle
+    canvas.drawCircle(Offset(centerX, centerY), radius, circlePaint);
+
+    // Calculate the angle based on the speed
+    final double maxSpeed = 20.0; // Adjust this based on your maximum expected speed
+    final double angle = (speed / maxSpeed) * 180.0;
+
+    // Draw the arrow
+    final double arrowLength = radius - 10;
+    final double arrowX = centerX + arrowLength * cos(degreesToRadians(-90 + angle));
+    final double arrowY = centerY + arrowLength * sin(degreesToRadians(-90 + angle));
+
+    canvas.drawLine(Offset(centerX, centerY), Offset(arrowX, arrowY), arrowPaint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
